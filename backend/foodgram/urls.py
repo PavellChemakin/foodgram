41b2bcabc2c83216cfe453986aacf5b229ee63ce
PavellChemakin <pavell-72@list.ru>
2025-08-23 21:27:26 +0500

    prod 9

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,9 +7,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
-    path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('recipes.urls')),
     path('api/', include('users.urls')),
 ]
