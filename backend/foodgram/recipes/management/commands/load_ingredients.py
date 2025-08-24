f7019e50e3bf85eaf477607a07ab303814d0a8f9
PavellChemakin <pavell-72@list.ru>
2025-08-24 18:10:20 +0500

    final prod.

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -3,9 +3,9 @@ import os
 
 from django.core.management.base import BaseCommand
 from progress.bar import IncrementalBar
+from recipes.models import Ingredient
 
 from foodgram import settings
-from recipes.models import Ingredient
 
 
 def ingredient_create(row):
